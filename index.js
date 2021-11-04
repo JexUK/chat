8034b857c098d6f803744f00d1745fc6fa53da14
Jex <cmrxn2005@gmail.com>
2021-11-04 16:55:14 +0000

    Initial commit

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
-var express = require('express'); // look for express in file
+// Import our external dependencies in order to let everything work.
+var express = require('express');
 var socket = require('socket.io');
 
-// App setup
+// Setup the Express application to get our HTML up.
 var app = express();
 var server = app.listen(9000, function(){
-    console.log('listening to requests on prt 9000');
+    console.log('[APP=>EXPRESS] JexChat is listening on port 9000.');
 });
 
+// A placeholder while waiting for messages to be send and recieved.
 var messages = [];
 
-// Static files
+// Static files for the Express app to display.
 app.use(express.static('public'));
 
-// Socket setup
+// Setup the Websocket to allow connections between multiple clients.
 var io = socket(server);
 io.on('connection', function(socket){
-    console.log('made socket connection', socket.id);
+    console.log('[INFO=>WS] Socket connection created with ID: ', socket.id);
 	
 	socket.emit('initial-connection', messages);
 	
@@ -23,9 +25,22 @@ io.on('connection', function(socket){
     // When I hear chat message sent to me, fire callback 
     // function to receive data and pass function
     socket.on('chat', function(data){
+
+        // Checks to make sure that data is valid to send to socket.
+        try {
+            if (!data.message || !data.handle) return console.error('Null message/handle sent to socket.');
+            if (data.message.length < 1) return console.error('Message too short to be sent to socket.');
+            if (data.handle.length < 3) return console.error('Handle too short to be sent to socket.');
+        } catch (e) { console.error(e) }
+
+        // Log the chat event to the console.
+        console.log('[APP=>MESSAGE] Message recieved: ' + data.message + ' at ' + new Date().toLocaleDateString());
+
 		messages.push(data);
         io.sockets.emit('chat', data);
     });
+
+    // Broadcast a typing event when someone on the app is typing.
 
     socket.on('typing', function(data){
         socket.broadcast.emit('typing', data);
--- public/chat.js
+++ public/chat.js
@@ -1,14 +1,14 @@
-// Make connection
+// Make the connection to the Websocket in order to do, well... everything.
 var socket = io.connect('http://localhost:9000');
 
-// Query DOM
+// Query the DOM HTML elements in order to recieve data.
 var message = document.getElementById('message'),
-      handle = document.getElementById('handle'),
-      btn = document.getElementById('send'),
-      output = document.getElementById('output'),
-      feedback = document.getElementById('feedback');
+    handle = document.getElementById('handle'),
+    btn = document.getElementById('send'),
+    output = document.getElementById('output'),
+    feedback = document.getElementById('feedback');
 
-// Emit events (upon click, emit to chat the message and handle)
+// Emit events to the Websocket.
 btn.addEventListener('click', function(){
     socket.emit('chat', {
         message: message.value,
@@ -32,11 +32,13 @@ socket.on('initial-connection', function(messages) {
 // Listen for events
 socket.on('chat', appendMessage);
 
+// If someone is typing, send that data to other active users.
 socket.on('typing', function(data){
-    feedback.innerHTML = '<p><em>' + data + ' is typing a message...</em></p>';
+    feedback.innerHTML = '<p><em>' + data + ' is typing...</em></p>';
 });
 
+// Retrieve old messages if a user is logging back in.
 function appendMessage(data) {
 	feedback.innerHTML = '';
-    output.innerHTML += '<p><strong>' + data.handle + ': </strong>' + data.message + '</p>';
+    output.innerHTML += '<p><strong>' + data.handle + '</strong> ' + data.message + '</p>';
 }
